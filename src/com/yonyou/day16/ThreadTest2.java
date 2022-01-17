eea1bc35c47708c93e1201bfc6074018b422de3b
wjp <wjp@hebau.edu>
2022-01-17 10:24:11 +0800

    javase commit2

--- src/com/yonyou/day16/ThreadTest2.java
+++ src/com/yonyou/day16/ThreadTest2.java
@@ -7,4 +7,22 @@ package com.yonyou.day16;
  *      线程的第二种创建方式
  */
 public class ThreadTest2 {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new MyThread2());
+        thread.start();
+        for (int i = 0; i <100 ; i++) {
+            System.out.println(Thread.currentThread().getName()+"开始运行   "+i);
+        }
+    }
+
+}
+
+class MyThread2 implements Runnable{
+
+    @Override
+    public void run() {
+        for (int i = 0; i <100 ; i++) {
+            System.out.println(Thread.currentThread().getName()+"开始运行   "+i);
+        }
+    }
 }
